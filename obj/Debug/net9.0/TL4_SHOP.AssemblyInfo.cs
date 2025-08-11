38e27baa5118913f1019732c15aa8b7697b8494b
ShuuOOO <nguyenminhdang013@gmail.com>
2025-08-11 19:36:28 +0700

    Đã fix xong form checkout, thay đổi font cho dễ đọc toàn bộ trang web

--- obj/Debug/net9.0/TL4_SHOP.AssemblyInfo.cs
+++ obj/Debug/net9.0/TL4_SHOP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TL4_SHOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb87b2ea2dbee6156da5f9bf9c1acf6c9eecd243")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9e54a8984617d8dbb0dbd6aeb6e1a75122ea641")]
 [assembly: System.Reflection.AssemblyProductAttribute("TL4_SHOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TL4_SHOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
