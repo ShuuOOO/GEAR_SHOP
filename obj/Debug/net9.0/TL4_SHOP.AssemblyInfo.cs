0f715d4e52957c2f8943f0289e3b571b1fa0519e
ShuuOOO <nguyenminhdang013@gmail.com>
2025-08-17 23:11:42 +0700

    FIX donhang admin

--- obj/Debug/net9.0/TL4_SHOP.AssemblyInfo.cs
+++ obj/Debug/net9.0/TL4_SHOP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TL4_SHOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c1043710eb55f4525aba605624ffb6d9c43bc2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e11607e5b81d7256bea088091697d5694680ff2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TL4_SHOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TL4_SHOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
